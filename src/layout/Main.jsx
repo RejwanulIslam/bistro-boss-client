4f33ce93bbe1a022fcde0c2f02af75392c3cf6c5
Rejwanul Islam <rejwanulislam45@gmail.com>
2025-06-04 14:09:25 +0600

    project setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../pages/shared/Footer'
+import Navbar from '../pages/shared/Navbar'
+
+export default function Main() {
+  return (
+    <div>
+      <Navbar></Navbar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+    </div>
+  )
+}
