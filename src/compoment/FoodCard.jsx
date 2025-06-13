a1570deb43cb0720f246c0f62497313f483b3b33
Rejwanul Islam <rejwanulislam45@gmail.com>
2025-06-13 23:15:55 +0600

    food card update

--- src/compoment/FoodCard.jsx
+++ src/compoment/FoodCard.jsx
@@ -8,7 +8,7 @@ export default function FoodCard({item}) {
             <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>{recipe}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Add to Card</button>
                 </div>
